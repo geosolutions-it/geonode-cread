6ec67ed85c6e92f143bc5f5053a18bd2774e1d0b
etj <etj@geo-solutions.it>
2015-07-27 16:43:27 +0200

    Fix url for geo upload

--- cread/urls.py
+++ cread/urls.py
@@ -15,13 +15,13 @@ urlpatterns = patterns(
         url(r'^/?$', TemplateView.as_view(template_name='site_index.html'), name='home'),
         url(r'^cread-upload/$', TemplateView.as_view(template_name='cread_upload.html'), name='cread_upload'),
         url(r'^cread-upload/doc/$', login_required(CreadDocumentUploadView.as_view()), name='cread_upload_doc'),
-        url(r'^cread-upload/geo/$', 'cread_upload_geo', name='cread_upload_geo'),
 
         url(r'', include(api.urls)),
         url(r'', include(override_api.urls)),
 
     ) + patterns(
         'cread.layers.views',  # py file name
+        url(r'^cread-upload/geo/$', 'cread_upload_geo', name='cread_upload_geo'),
         url(r'^layers/(?P<layername>[^/]*)/cread_metadata_update$', 'layer_metadata_update', name="cread_layer_metadata_update"),
         url(r'^layers/(?P<layername>[^/]*)/cread_metadata_create$', 'layer_metadata_create', name="cread_layer_metadata_create"),
 
