64e7a97f527ca896951f7a31267dbb4f530bc79d
root <root@server-84-33-2-79.static.serverclienti.com>
2015-07-27 15:26:07 +0000

    - fix a minor url mapping bug

--- cread/urls.py
+++ cread/urls.py
@@ -15,7 +15,6 @@ urlpatterns = patterns(
         url(r'^/?$', TemplateView.as_view(template_name='site_index.html'), name='home'),
         url(r'^cread-upload/$', TemplateView.as_view(template_name='cread_upload.html'), name='cread_upload'),
         url(r'^cread-upload/doc/$', login_required(CreadDocumentUploadView.as_view()), name='cread_upload_doc'),
-        url(r'^cread-upload/geo/$', 'cread_upload_geo', name='cread_upload_geo'),
 
         url(r'', include(api.urls)),
         url(r'', include(override_api.urls)),
@@ -24,6 +23,7 @@ urlpatterns = patterns(
         'cread.layers.views',  # py file name
         url(r'^layers/(?P<layername>[^/]*)/cread_metadata_update$', 'layer_metadata_update', name="cread_layer_metadata_update"),
         url(r'^layers/(?P<layername>[^/]*)/cread_metadata_create$', 'layer_metadata_create', name="cread_layer_metadata_create"),
+        url(r'^cread-upload/geo/$', 'cread_upload_geo', name='cread_upload_geo'),
 
     ) + patterns(
         'cread.documents.views',  # py file name
